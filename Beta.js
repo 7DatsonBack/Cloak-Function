9b5a73e1a2816969ed384036438032b501571972
7DatsonBack <124845332+7DatsonBack@users.noreply.github.com>
2023-04-20 09:26:04 -0700

    Create Beta.js

---
+++ Beta.js
@@ -0,0 +1,24 @@
+function link() {
+    
+    const linkValue = prompt("Enter your link:");
+    if (linkValue) {a(linkValue); } function a(link) {
+        alert('Iframe Started');
+        const blank = window.open("about:blank", "_blank");
+        const doc = blank.document;
+        const iframe = doc.createElement("iframe");
+        const style = iframe.style;
+        const img = doc.createElement("link")
+        doc.title = "Canvas";
+        img.rel = "icon";
+        img.href = "https://ssl.gstatic.com/classroom/favicon.png";
+        iframe.src = link;
+        style.position = "fixed";
+        style.border = style.width = "none";
+        style.width = style.height = "100%";
+        style.top = style.bottom = style.left = style.right = 0;
+        iframe.setAttribute('allowFullScreen', '');
+        doc.body.appenChild(iframe);
+    }  
+}
+
+link();
