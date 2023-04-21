96543ca244613f0fbf36196c58ea7ca2a27b0090
7DatsonBack <124845332+7DatsonBack@users.noreply.github.com>
2023-04-20 20:35:31 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/src/handler.js',
-    bundle: '/src/bundle.js',
-    config: '/src/config.js',
-    sw: '/src/sw.js',
+    handler: '/src/uv.handler.js',
+    bundle: '/src/uv.bundle.js',
+    config: '/src/uv.config.js',
+    sw: '/src/uv.sw.js',
 };
