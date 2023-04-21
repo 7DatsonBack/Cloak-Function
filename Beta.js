58633d47e76826e521cb5ff69dd8c218d2d0bb9d
7DatsonBack <124845332+7DatsonBack@users.noreply.github.com>
2023-04-20 20:32:19 -0700

    Update Beta.js

--- Beta.js
+++ Beta.js
@@ -1,24 +1,29 @@
+import { __uv$config } from './config.js';
+
 function link() {
-    
-    const linkValue = prompt("Enter your link:");
-    if (linkValue) {a(linkValue); } function a(link) {
-        alert('Iframe Started');
-        const blank = window.open("about:blank", "_blank");
-        const doc = blank.document;
-        const iframe = doc.createElement("iframe");
-        const style = iframe.style;
-        const img = doc.createElement("link")
-        doc.title = "Canvas";
-        img.rel = "icon";
-        img.href = "https://ssl.gstatic.com/classroom/favicon.png";
-        iframe.src = link;
-        style.position = "fixed";
-        style.border = style.width = "none";
-        style.width = style.height = "100%";
-        style.top = style.bottom = style.left = style.right = 0;
-        iframe.setAttribute('allowFullScreen', '');
-        doc.body.appendChild(iframe);
-    }  
+  const linkValue = prompt("Enter your link:");
+  if (linkValue) {
+    let url = linkValue.trim();
+    if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+    else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+    window.navigator.serviceWorker.register('./sw.js', {
+      scope: __uv$config.prefix
+    }).then(() => {
+      window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+  }
 }
 
-link();
+function isUrl(val = '') {
+  if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+  return false;
+}
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+  link();
+});
